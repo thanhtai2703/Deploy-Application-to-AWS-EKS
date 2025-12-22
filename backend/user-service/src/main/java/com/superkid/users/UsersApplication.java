71d9eabce4b60e1146ff94567a4f9bba73f6fd68
Truong Thanh Tai <thanhtai05.work@gmail.com>
2025-12-22 16:01:53 +0700

    testing

--- backend/user-service/src/main/java/com/superkid/users/UsersApplication.java
+++ backend/user-service/src/main/java/com/superkid/users/UsersApplication.java
@@ -2,7 +2,7 @@ package com.superkid.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
 @SpringBootApplication
 public class UsersApplication {
 
